ae99b8d44ffd2da18c87d5ac737f68b442cc70f9
Immanuel Wiessler <im.wiessler@gmail.com>
2023-03-24 17:32:01 -0700

    added a new function that when exit button is pressed that it closes it when exit button is pressed

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from dearpygui import  core, simple
 def myfunction(sender,data):
     print("Sender : ", sender)
     print("Data :", data)
+
+def exitProgramFunc(sender,data):
+    print(data)
+    if data=="ExitGui":
+        core.stop_dearpygui()
+
 def myfunction2(sender,data):
     userName=core.get_value("username")
     password=core.get_value("password")
     print(f"username : {userName}")
     print(f"Password : {password}")
 
-dsds
+
 ##simple.show_style_editor()
-core.set_theme("Light")
+core.set_theme("Classic")
 ##simple.show_documentation()
 
 
@@ -26,4 +32,5 @@ with simple.window("TEST Win 2",autosize=True,x_pos=150,y_pos=200):
     core.add_input_text(name="username",hint="Enter username",on_enter=True, callback=myfunction2)
     core.add_input_text(name="password",hint="Enter password",password=True, on_enter=True, callback=myfunction2)
     core.add_button("Send",callback=myfunction2)
+    core.add_button("Exit",callback_data="ExitGui",callback=exitProgramFunc)
 core.start_dearpygui()
